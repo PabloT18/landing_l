63d83536e55c0acb4723c6105bae5c2505a47bd9
PabloTorres <ptorresp@ups.edu.ec>
2025-02-14 13:53:39 -0500

    ADD projrect

--- lib/features/home/domain/models/song.dart
+++ lib/features/home/domain/models/song.dart
@@ -1,22 +1,26 @@
-import 'dart:async';
-
 import 'package:equatable/equatable.dart';
 
 class Song extends Equatable {
   const Song({
     required this.title,
-    required this.isSelected,
+    this.isSelected = false,
+    required this.filePath,
   });
   final String title;
   final bool isSelected;
+  final String filePath;
 
   @override
-  List<Object?> get props => [title, isSelected];
+  List<Object?> get props => [title, isSelected, filePath];
 
-  Song copyWith({String? title, bool? isSelected}) {
+  Song copyWith({
+    String? title,
+    bool? isSelected,
+  }) {
     return Song(
       title: title ?? this.title,
       isSelected: isSelected ?? this.isSelected,
+      filePath: filePath,
     );
   }
 }
--- lib/features/home/presentation/bloc/song_bloc/song_bloc_bloc.dart
+++ lib/features/home/presentation/bloc/song_bloc/song_bloc_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:landing_l/features/home/domain/models/song.dart';
@@ -10,20 +11,37 @@ part 'song_bloc_state.dart';
 class SongBloc extends Bloc<SongEvent, SongState> {
   SongBloc()
       : super(SongState(canciones: [
-          Song(title: 'Cama y Mesa', isSelected: false),
-          Song(title: 'Celos', isSelected: false),
-          Song(title: 'Eres Mi Persona Favorita', isSelected: false),
-          Song(title: 'Aprender a Quererte', isSelected: false),
-          Song(title: 'Si Nos Dejan', isSelected: false),
-          Song(title: 'Día Tras Día', isSelected: false),
-          Song(title: 'Bendita Tu Luz', isSelected: false),
-          Song(title: 'Río', isSelected: false),
+          Song(
+              title: 'Cama y Mesa',
+              filePath:
+                  'https://drive.google.com/uc?export=download&id=1jraNn-QDyaHEQ59n1XpdTu_5qUM0hHBG'),
+          Song(
+              title: 'Celos',
+              filePath:
+                  'https://drive.google.com/file/d/1jraNn-QDyaHEQ59n1XpdTu_5qUM0hHBG/view?usp=sharing'),
+          Song(
+              title: 'Eres Mi Persona Favorita',
+              filePath: 'assets/songs/eres_mi_persona_favorita.mp3'),
+          Song(
+              title: 'Aprender a Quererte',
+              filePath: 'assets/songs/aprender_a_quererte.mp3'),
+          Song(
+              title: 'Si Nos Dejan', filePath: 'assets/songs/si_nos_dejan.mp3'),
+          Song(
+              title: 'Día Tras Día', filePath: 'assets/songs/dia_tras_dia.mp3'),
+          Song(
+              title: 'Bendita Tu Luz',
+              filePath: 'assets/songs/bendita_tu_luz.mp3'),
+          Song(title: 'Río', filePath: 'assets/songs/rio.mp3'),
+          Song(title: 'Imagínate', filePath: 'assets/songs/imaginate.mp3'),
         ], cancioneSeleccionada: null)) {
     on<SongSelected>(_onSongSelected);
 
     // Seleccionar la primera canción automáticamente
     add(SongSelected(state.canciones.first));
   }
+
+  final AudioPlayer _audioPlayer = AudioPlayer(); // Instancia de AudioPlayer
 
   FutureOr<void> _onSongSelected(
       SongSelected event, Emitter<SongState> emit) async {
@@ -34,7 +52,23 @@ class SongBloc extends Bloc<SongEvent, SongState> {
         return e.copyWith(isSelected: false);
       }
     }).toList();
+
     emit(
         state.copyWith(canciones: canciones, cancioneSeleccionada: event.song));
+
+    // Reproducir la nueva canción
+    await _playSong(event.song.filePath);
+  }
+
+  Future<void> _playSong(String filePath) async {
+    await _audioPlayer.stop(); // Detener la canción actual
+    // await _audioPlayer.play(AssetSource(filePath)); // Reproducir la nueva
+    await _audioPlayer.play(UrlSource(filePath));
+  }
+
+  @override
+  Future<void> close() {
+    _audioPlayer.dispose(); // Liberar recursos del audio
+    return super.close();
   }
 }
